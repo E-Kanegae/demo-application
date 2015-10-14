030ba1750f271a298aeb3b3cec9d30610931e0a5
Kazuki Shimizu <kazuki43zoo@gmail.com>
2015-10-14 14:56:48 +0900

    Change random

--- startup-cargo-daemon.sh
+++ startup-cargo-daemon.sh
@@ -4,4 +4,4 @@ export CARGO_DAEMON_WEBAPP_VERSION=1.4.16
 if test ! -f $HOME/cargo-daemon/cargo-daemon-webapp-${CARGO_DAEMON_WEBAPP_VERSION}.war ; then
     mvn dependency:copy -Dartifact=org.codehaus.cargo:cargo-daemon-webapp:${CARGO_DAEMON_WEBAPP_VERSION}:war -DoutputDirectory=$HOME/cargo-daemon/.
 fi
-java -Djava.security.egd=file:/dev/urandom -jar $HOME/cargo-daemon/cargo-daemon-webapp-${CARGO_DAEMON_WEBAPP_VERSION}.war &
+java -Djava.security.egd=file:/dev/random -jar $HOME/cargo-daemon/cargo-daemon-webapp-${CARGO_DAEMON_WEBAPP_VERSION}.war &
